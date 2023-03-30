740c6a5b9a422598d467a0e741518a492667392e
moon-July5 <rudtjr0705@naver.com>
2023-03-30 23:39:49 +0900

    [문경석] #security - FEAT/cors 설정

--- src/main/java/com/group4/miniproject/config/SecurityConfig.java
+++ src/main/java/com/group4/miniproject/config/SecurityConfig.java
@@ -23,6 +23,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Log4j2
 @RequiredArgsConstructor
@@ -47,6 +50,22 @@ public class SecurityConfig {
 //    AuthenticationManager authenticationManager(AuthenticationManagerBuilder builder) throws Exception {
 //        return builder.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder()).and().build();
 //    }
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.addAllowedOrigin("http://localhost:3000");
+        configuration.addAllowedMethod("*");
+        configuration.addAllowedHeader("*");
+        configuration.addExposedHeader("Authorization");
+        configuration.setAllowCredentials(true);
+        configuration.setMaxAge(3600L);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder(){
@@ -59,7 +78,8 @@ public class SecurityConfig {
         return http
                 .httpBasic().disable()
                 .formLogin().disable()
-                .cors().disable()
+                .cors()
+                .and()
                 .csrf().disable()
                 .headers().frameOptions().disable()
                 .and()
