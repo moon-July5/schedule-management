8a8ec5ed2d12c5799b15c000b63743df887fa3a4
gh999 <gh934@naver.com>
2023-03-30 16:48:56 +0900

    [권명정] #loginResponse - fix : response수정

--- src/main/java/com/group4/miniproject/dto/AccountLoginResponseDTO.java
+++ src/main/java/com/group4/miniproject/dto/AccountLoginResponseDTO.java
@@ -1,37 +1,89 @@
 package com.group4.miniproject.dto;
 
+import com.group4.miniproject.domain.Account;
 import com.group4.miniproject.domain.AccountRole;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.ToString;
+import com.group4.miniproject.domain.Schedule;
+import com.group4.miniproject.domain.ScheduleType;
+import lombok.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
-@ToString
-@Getter
 @Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class AccountLoginResponseDTO {
-    private String name;
-
-    private String accountId;
-
-    private Set<AccountRole> accountRole;
-
-    private String email;
-
-    private String department;
-
-    private String position;
-
-    private Long yearly;
-
-    boolean duty;
-
-    private List<Long> scheduleId ;
-
+    private UserData user;
+    private List<ScheduleData> schedules;
     private String JWTToken;
+    @Getter
+    @Setter
+    public static class UserData {
+        private Long id;
+        private String name;
 
+        private String accountId;
+
+        private Set<AccountRole> role;
+
+        private String email;
+
+        private String department;
+
+        private String position;
+
+        private Long yearly;
+
+        boolean duty;
+
+        public UserData(Account a) {
+            this.id = a.getId();
+            this.name = a.getName();
+            this.accountId = a.getAccountId();
+            this.role = a.getRoles();
+            this.email = a.getEmail();
+            this.department = a.getDepartment();
+            this.position = a.getPosition();
+            this.yearly = a.getYearly();
+            this.duty = a.getDuty();
+        }
+    }
+
+
+    @Getter
+    @Setter
+    public static class ScheduleData {
+        private Long id;
+        private String accountId;
+        private ScheduleType type;
+        private String content;
+        private LocalDateTime start_date;
+        private LocalDateTime end_date;
+        private LocalDateTime created_at;
+        private LocalDateTime modified_at;
+
+        public ScheduleData(Schedule s) {
+            this.id = s.getId();
+            this.accountId = s.getAccount().getAccountId();
+            this.type = s.getType();
+            this.content = s.getContent();
+            this.start_date = s.getStartDate();
+            this.end_date = s.getEndDate();
+            this.created_at = s.getCreatedAt();
+            this.modified_at = s.getModifiedAt();
+        }
+    }
+
+    public static AccountLoginResponseDTO from( Account account,String jwtToken) throws Exception {
+        return AccountLoginResponseDTO.builder()
+                .user(new UserData(account))
+                .JWTToken(jwtToken)
+                .schedules(account.getSchedules().stream().map(ScheduleData::new).collect(Collectors.toList()))
+                .build();
+    }
 }
 /*
 * "name": "Nicolas Serrano Arevalo",
