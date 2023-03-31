b98c87ca09a46ebd4dafd05d3a6f6e5270e242cf
moon-July5 <rudtjr0705@naver.com>
2023-03-31 19:19:54 +0900

    [문경석] #Schedule - FEAT/scheduleTodayRequestDTO코드 수정

--- src/main/java/com/group4/miniproject/dto/ScheduleTodayRequestDTO.java
+++ src/main/java/com/group4/miniproject/dto/ScheduleTodayRequestDTO.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
 @Builder
@@ -11,8 +12,10 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ScheduleTodayRequestDTO {
+    @DateTimeFormat(pattern = "yyyy-MM-dd kk:mm:ss")
     private LocalDateTime start_date;
 
+    @DateTimeFormat(pattern = "yyyy-MM-dd kk:mm:ss")
     private LocalDateTime end_date;
 }
 //start_date:현재 날짜, end_date:현재 날짜}
